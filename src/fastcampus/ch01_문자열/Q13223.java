a49399be8da225cac94065d2578bb5d84d86e5f9
minkyung kim <minry20415@naver.com>
2024-12-05 22:11:53 +0900

    [Q13223] 소금 폭탄

---
+++ src/fastcampus/ch01_문자열/Q13223.java
@@ -0,0 +1,39 @@
+package fastcampus.ch01_문자열;
+
+import java.util.Scanner;
+
+public class Q13223 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String current = sc.next();
+        String drop = sc.next();
+
+        String[] currentUnit = current.split(":");
+        int currentHour = Integer.parseInt(currentUnit[0]);
+        int currentMinute = Integer.parseInt(currentUnit[1]);
+        int currentSecond = Integer.parseInt(currentUnit[2]);
+        int currentSecondAmount = currentHour * 3600 + currentMinute * 60 + currentSecond;
+
+        String[] dropUnit = drop.split(":");
+        int dropHour = Integer.parseInt(dropUnit[0]);
+        int dropMinute = Integer.parseInt(dropUnit[1]);
+        int dropSecond = Integer.parseInt(dropUnit[2]);
+        int dropSecondAmount = dropHour * 3600 + dropMinute * 60 + dropSecond;
+
+        // 필요한 시간
+        int needSecondAmount = dropSecondAmount - currentSecondAmount;
+        if (needSecondAmount <= 0) {
+            needSecondAmount += 24 * 3600;
+        }
+
+        int needHour = needSecondAmount / 3600;
+        int needMinute = (needSecondAmount % 3600) / 60;
+        int needSecond = needSecondAmount % 60;
+
+        System.out.printf("%02d:%02d:%02d", needHour, needMinute, needSecond);
+
+
+
+    }
+}
