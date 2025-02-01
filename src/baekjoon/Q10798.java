67a85d1532945bae9bfa9150a0ea92055d1f64ce
minkyung kim <minry20415@naver.com>
2025-02-01 22:23:26 +0900

    [Q10798] 세로읽기

---
+++ src/baekjoon/Q10798.java
@@ -0,0 +1,36 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q10798 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char[][] ch = new char[5][15];
+        int max = 0;
+
+        for (int i = 0; i < ch.length; i++) {
+            String str = sc.next();
+            if (max < str.length()) {
+                max = str.length();
+            }
+
+            for (int j = 0; j < str.length(); j++) {
+                ch[i][j] = str.charAt(j);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < max; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (ch[j][i] == '\0') {
+                    continue;
+                }
+                sb.append(ch[j][i]);
+            }
+        }
+        System.out.println(sb);
+    }
+
+}
